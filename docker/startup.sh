f6962ed2e685f824de1db536e812169d4fa3c44b
Chris Troutner <chris.troutner@gmail.com>
2019-07-15 08:30:18 -0700

    Removing reference to Insight API. Not needed for websockets

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,9 +8,4 @@ export ZEROMQ_PORT=28331
 export ZEROMQ_URL=localhost
 export NETWORK=testnet
 
-#export BITDB_URL=https://tbitdb.bitcoin.com/
-
-# Team DO QA Testnet Insight Server
-export BITCOINCOM_BASEURL=http://localhost:3001/api/
-
 npm start
